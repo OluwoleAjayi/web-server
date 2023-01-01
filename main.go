75db56663660e7dde69194be45ac8b76c4572e4f
oluwole <oluwoleajayijnr@gmail.com>
2023-01-01 16:46:25 +0100

    Added functionality for a web server

--- main.go
+++ main.go
@@ -1,26 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main() {
-	arr := [...]int{3, 42, 2}
+func formHandler(w http.ResponseWriter, r *http.Request) {
 
-	// for i := 1; i < 100; i++ {
-	// 	fmt.Println(fizzBuzz(i))
-	// }
-	fmt.Print(arr)
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+
 }
 
-// func fizzBuzz(num int) string {
-// 	switch {
-// 	case num%3 == 0:
-// 		fmt.Println("Fizz")
-// 	case num%5 == 0:
-// 		fmt.Println("Buzz")
-// 	case num%15 == 0:
-// 		fmt.Println("FizzBuzz")
-// 	}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
 
-// 	return strconv.Itoa(num)
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
 
-// }
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
